cd812e2d47615d5d9801645a3b1b7ffaf19f14dd
Zal-Atan <ethanzalta@gmail.com>
2023-12-08 11:00:14 -0600

    Task 4 working, how I understand it ¯\_(ツ)_/¯

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/3-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -37,7 +37,7 @@ def hbnb():
 
     cache_id = uuid.uuid4()
 
-    return render_template('0-hbnb.html',
+    return render_template('3-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places, cache_id=cache_id)
@@ -45,4 +45,4 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
